22772bda183d0a3f3cb6d10e500559e4d3ddb601
Ziqiao Wang <wzqjanet@gmail.com>
2020-02-04 15:31:37 -0600

    Add files via upload

---
+++ data/data.R
@@ -0,0 +1,15 @@
+#' Example data for the vignettes
+#'
+#' A dataset with summary statistics p values of 1000 genes for RNAseq and CNV data
+#'
+#' @docType data
+#'
+#' @usage data(data_p)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @source Example data
+#'
+"data_p"
